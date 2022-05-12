6989ee84ae1d04aa4b2d9b825b8592a6f9d70dc7
KajPe <15047180+KajPe@users.noreply.github.com>
2022-05-12 20:43:23 +0300

    Fix backdrop z-index for each modal
    Now the z-index gets fixed for each stacked modal.

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const modal_base_idx = 10000;
+let modal_idx = 1;
+
 Array.from(document.getElementsByClassName('showmodal')).forEach( (e) => {
   e.addEventListener('click', function(element) {
     element.preventDefault();
@@ -7,6 +10,27 @@ Array.from(document.getElementsByClassName('showmodal')).forEach( (e) => {
       showModal(e.getAttribute('data-show-modal'));
     }
   }); 
+});
+
+// This all is just to get modals stacked on each-other by manipulating z-index
+Array.from(document.getElementsByClassName('modal')).forEach( (e) => {
+  e.addEventListener('shown.bs.modal', function(event) {
+    // Set the current level of modal
+    e.setAttribute('data-idx', modal_idx);
+    e.style.zIndex = modal_base_idx + (modal_idx * 100);
+    // Find the backdrop and sets it z-index
+    Array.from(document.getElementsByClassName('modal-backdrop')).forEach( (element) => {
+      if (!element.hasAttribute('data-idx')) {
+        element.setAttribute('data-idx', modal_idx);
+        element.style.zIndex = modal_base_idx + (modal_idx * 100) - 10;
+      }
+    });
+    modal_idx += 1;
+  });
+  e.addEventListener('hidden.bs.modal', function(event) {
+    // Modal closed
+    modal_idx -= 1;
+  });
 });
 
 // Show modal dialog
